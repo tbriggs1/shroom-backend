e638c604ab5e981a706ea8c2abae4d902e4d73b7
tbriggs1 <tombriggs1993@outlook.com>
2023-08-21 21:11:31 +0100

    Added cors

--- shroom/settings.py
+++ shroom/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'backend',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'shroom.urls'
@@ -71,6 +73,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'shroom.wsgi.application'
 
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Add the URL of your React app
+]
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
