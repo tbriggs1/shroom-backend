6e293c1ed6b143aa0509b19f16dfb8267a1b9c75
tbriggs1 <tombriggs1993@outlook.com>
2023-08-23 21:45:01 +0100

    allowing for cors

--- shroom/settings.py
+++ shroom/settings.py
@@ -47,12 +47,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'shroom.urls'
@@ -75,10 +75,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'shroom.wsgi.application'
 
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "jenkins.tomjbriggs.com",
-    "46.105.174.113",
     "*"
 ]
 
